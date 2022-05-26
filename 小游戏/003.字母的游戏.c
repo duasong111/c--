5021872ce40429bd61758cefdd825c48b6a769e0
duasong111 <98256947+duasong111@users.noreply.github.com>
2022-05-26 22:53:47 +0800

    Create 003.字母的游戏.c

---
+++ 小游戏/003.字母的游戏.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
+#define LEVEL_MIN 3
+#define LEVEL_MAX 20
+int main(void)
+{
+	int stage;
+	printf("请输入你想输入随机数的位数3-20位");
+	scanf_s("%d", &stage);
+	const char ltr[] = "abcdefghijklmnopoe";
+	srand(time(NULL));
+	for (int j = 1; j < stage; j++) {
+		char no[LEVEL_MAX + 1];
+		char x[LEVEL_MAX * 2];
+
+		//no[0] = '1' + rand() % 9;
+		for (int i = 1; i < stage; i++) {
+			no[i] = ltr[rand() % ltr]  //都是随机数，也没法限制啊
+		}
+		no[stage] = '\0';  //没有这个说明则最后一位是没有结束语的
+
+		printf("%s\n", no);
+	}
+	return 0;
+}
